ccd28176f96dd36754766cf0baa68797c7e98803
Ali Nasrollahi <a.nasrollahi@arad.co.net>
2023-03-27 16:10:12 +0330

    major changes: 1. designing ui of the program 2. making signal-slot relationship for Browse and Cancel buttons in the ui 3. the signal-slot relationship for Import button is not complete (checking    for completeness of ui's sections and validating user inputs have been done,    but the main duty of the button is not implemented)

--- include/shp2postgresql.h
+++ include/shp2postgresql.h
@@ -1,7 +1,11 @@
 #ifndef SHAPEFILECONVERSION_SHP2POSTGRESQL_H
 #define SHAPEFILECONVERSION_SHP2POSTGRESQL_H
 
+#include <../res/ui_shp2postgresql.h>
+
 #include <QMainWindow>
+
+#include <memory>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Shp2Postgresql; }
@@ -16,10 +20,35 @@ namespace ShapefileConversion
 
     public:
         Shp2Postgresql(QWidget *parent = nullptr);
-        ~Shp2Postgresql();
+        virtual ~Shp2Postgresql() = default;
+
+    protected:
+        /// @brief This method checks whether all sections in the form
+        ///        are filled or not .
+        /// @return If all fields have been filled, this method returns true,
+        ///         otherwise, it throws exception (std::runtime_error
+        ///         exception) and says which field hasn't been filled.
+        bool allSectionsFilled() const;
+
+        /// @brief This method checks whether user inputs are valid or not
+        ///        (user input means the values which user has written in
+        ///        the form sections) .
+        /// @return Like "allSectionFilled()" method .
+        bool userInputsAreValid() const;
 
     private:
-        Ui::Shp2Postgresql *ui;
+        std::unique_ptr<Ui::Shp2Postgresql> _ui;
+
+    private slots:
+        /// @brief When "Browse" push button (shapefile path field) clicked,
+        ///        this slot will be invoked
+        void slot_shpPathBrowseButton();
+
+        /// @brief When "Cancel" push button clicked, this slot will be invoked
+        void slot_cancelButtonClicked();
+
+        /// @brief When "Import" push button clicked, this slot will be invoked
+        void slot_importButtonClicked();
     };
 
 }
--- src/shp2postgresql.cpp
+++ src/shp2postgresql.cpp
@@ -1,15 +1,133 @@
 #include "../include/shp2postgresql.h"
-#include "../res/ui_shp2postgresql.h"
+
+#include <QFileDialog>
+#include <QString>
+#include <QMessageBox>
+#include <QFile>
+
+#include <stdexcept>
+#include <regex>
+#include <stdio.h>
 
 ShapefileConversion::Shp2Postgresql::Shp2Postgresql(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::Shp2Postgresql)
+    : QMainWindow(parent),
+      _ui(std::make_unique<Ui::Shp2Postgresql>())
 {
-    ui->setupUi(this);
+    this->_ui->setupUi(this);
+    this->setFixedSize(this->geometry().width(), this->geometry().height());
+
+
+    QObject::connect(this->_ui->shapefilePathBrowseButton, SIGNAL(clicked()), this, SLOT(slot_shpPathBrowseButton()));
+    QObject::connect(this->_ui->cancelButton, SIGNAL(clicked()), this, SLOT(slot_cancelButtonClicked()));
+    QObject::connect(this->_ui->importButton, SIGNAL(clicked()), this, SLOT(slot_importButtonClicked()));
 }
 
-ShapefileConversion::Shp2Postgresql::~Shp2Postgresql()
+bool ShapefileConversion::Shp2Postgresql::allSectionsFilled() const
 {
-    delete ui;
+    if (this->_ui->shapefilePathLineEdit->text().isEmpty())
+        throw std::runtime_error("You Should Fill Shapefile Path Section");
+
+    else if (this->_ui->dbNameLineEdit->text().isEmpty())
+        throw std::runtime_error("You Should Fill Database Name Section");
+
+    else if (this->_ui->tableNameLineEdit->text().isEmpty())
+        throw std::runtime_error("You Should Fill Table Name Section");
+
+    else if (this->_ui->hostIpAddrLineEdit->text().isEmpty())
+        throw std::runtime_error("You Should Fill Host IP Address Section");
+
+    else if (this->_ui->usernameLineEdit->text().isEmpty())
+        throw std::runtime_error("You Should Fill Username Section");
+
+    return true;
 }
 
+bool ShapefileConversion::Shp2Postgresql::userInputsAreValid() const
+{
+    // Validating shapefile path (the input file exists or not)
+    QFile file(this->_ui->shapefilePathLineEdit->text().trimmed());
+    if (!file.exists())
+        throw std::runtime_error("The Input Shapefile Path Doesn't Exists");
+
+    // validating input database name
+    std::regex pgsqlValidDBNamePattern("^[A-Za-z][A-Za-z0-9_]{0,62}$");
+    if (!std::regex_match(this->_ui->dbNameLineEdit->text().trimmed().toStdString(), pgsqlValidDBNamePattern))
+        throw std::runtime_error("The Input Database Name Is Not A Valid DB Name in PostgreSQL");
+
+    // validating input table name
+    std::regex pgsqlValidTableNamePattern("^[A-Za-z][A-Za-z0-9_]{0,62}$");
+    if (!std::regex_match(this->_ui->tableNameLineEdit->text().trimmed().toStdString(), pgsqlValidTableNamePattern))
+        throw std::runtime_error("The Input Table Name Is Not A Valid Table Name in PostgreSQL");
+
+    // validating ip address syntax
+    // Regex to validate IP address format
+    std::regex validIpPattern("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$");
+    if (!std::regex_match(this->_ui->hostIpAddrLineEdit->text().trimmed().toStdString(), validIpPattern))
+        throw std::runtime_error("Invalid Syntax For Host IP Address");
+
+
+    // Validating input username
+    char* osName;
+
+#if defined(__linux__)
+    // linux username pattern
+    std::regex validUsernamePattern("^[a-z_][a-z0-9_-]{0,31}$");
+    osName = "Linux";
+
+    // windows username pattern
+#elif defined(_WIN32) || defined(_WIN64)
+    std::regex validUsernamePattern("^[a-zA-Z][a-zA-Z0-9._-]{0,19}$");
+    osName = "Windows";
+
+    // osx (macOS) username pattern
+#elif defined(__APPLE__)
+    std::regex validUsernamePattern("^[a-zA-Z][a-zA-Z0-9._-]{0,30}$");
+    osName = "MacOS";
+#endif
+
+    if (!std::regex_match(this->_ui->usernameLineEdit->text().trimmed().toStdString(), validUsernamePattern))
+    {
+        char* message;
+        sprintf(message, "The input username is not a valid username for %s system", osName);
+            throw std::runtime_error(message);
+    }
+}
+
+void ShapefileConversion::Shp2Postgresql::slot_shpPathBrowseButton()
+{
+    QString filePath = QFileDialog::getOpenFileName(
+                nullptr,
+                QString("Select Shapefile"),
+                QDir::homePath(),
+                QString("Shp (*.shp)")
+    );
+
+    if (!filePath.isEmpty())
+        this->_ui->shapefilePathLineEdit->setText(filePath);
+}
+
+void ShapefileConversion::Shp2Postgresql::slot_cancelButtonClicked()
+{
+    this->close();
+}
+
+void ShapefileConversion::Shp2Postgresql::slot_importButtonClicked()
+{
+    try
+    {
+        if (this->allSectionsFilled() && this->userInputsAreValid())
+            ;
+    }
+    catch(std::runtime_error const& ex)
+    {
+        QMessageBox::critical(nullptr, "Runtime Error", ex.what());
+    }
+    catch(std::exception const& ex)
+    {
+        QMessageBox::critical(nullptr, "Exception Occured!", ex.what());
+    }
+    catch(...)
+    {
+        QMessageBox::critical(nullptr, "Unknown Error!", "Something Went wrong");
+    }
+}
